e1980f4b5be443dbba9c1fc4053695db73e365b6
vivek rai <vivek77rai@gmail.com>
2022-04-17 22:12:41 +0530

    commit for showcasing conflict

--- src/BitsAssignment.java
+++ src/BitsAssignment.java
@@ -1,6 +1,7 @@
 
 public class BitsAssignment {
 public static void main(String args[]) {
-	System.out.println("My sample git project :)");
+	int commit=2;
+	System.out.println("My sample git project :)"+commit);
 }
 }
